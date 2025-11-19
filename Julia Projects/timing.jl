c4521197783bbfc762d6c647760e7b4fec020b2c
Jolt8 <willemartin@icloud.com>
2025-11-19 10:18:52 -0600

    created a seperate timing file for future sims

---
+++ Julia Projects/timing.jl
@@ -0,0 +1,4 @@
+
+#C:\Users\wille\Desktop\Legacy-Projects-And-Code-Will-Martin\Julia Projects\Testing FEM with mtkcomponents.jl
+@time include("C://Users//wille//Desktop//Legacy-Projects-And-Code-Will-Martin//Julia Projects//Testing FEM with mtkcomponents.jl")
+#10x10 takes 4.77 seconds
