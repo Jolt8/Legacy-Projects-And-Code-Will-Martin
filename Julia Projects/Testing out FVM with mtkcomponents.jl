cb7b6539519b83b2afc7c6a0c7b13e6728467c5f
Jolt8 <willemartin@icloud.com>
2025-11-19 10:18:19 -0600

    Getting closer to finishing this even though I'm doubting its scalability

--- Julia Projects/Testing out FVM with mtkcomponents.jl
+++ Julia Projects/Testing out FVM with mtkcomponents.jl
@@ -26,10 +26,11 @@ end
         port = HeatPort()
     end
     @parameters begin
-        k = 1.0, [description = "thermal conductivity [W/(m*k)]"]
-        A = 1.0, [description = "facet area [m^2]"]
-        d = 1.0, [description = "distance between centroids [m]"]
-        C = k * (A / d), [description = "Heat capacity of element [J/(g*K)]"]
+        #k = 1.0, [description = "thermal conductivity [W/(m*k)]"]
+        #A = 1.0, [description = "facet area [m^2]"]
+        #d = 1.0, [description = "distance between centroids [m]"]
+        C = 1.0 
+        #k * (A / d), [description = "Heat capacity of element [J/(g*K)]"]
     end
     @variables begin
         T(t) = 293.15, [description = "Temperature of element [K]"]
@@ -55,6 +56,7 @@ end
         dT ~ port_a.T - port_b.T
         port_a.Q_flow ~ Q_flow
         port_a.Q_flow + port_b.Q_flow ~ 0
+        #Q_flow ~ G * dT
     end
 end
 
@@ -99,53 +101,83 @@ end
 left = Ferrite.Vec{3}((0.0, 0.0, 0.0))
 right = Ferrite.Vec{3}((1.0, 1.0, 1.0))
 
-grid = generate_grid(Hexahedron, (2, 2, 2), left, right)
+grid = generate_grid(Hexahedron, (4, 1, 4), left, right)
 
-addcellset!(grid, "left", (x) -> x[1] ≈ 0)
-addcellset!(grid, "right", (x) -> x[1] ≈ 1)
+addcellset!(grid, "left", x -> x[1] <= 0.25)
+getcellset(grid, "left")
+addcellset!(grid, "right", (x) -> x[1] >= 0.75)
+getcellset(grid, "right")
 
-function generate_FVM_connectivity(grid, poly_interp, cell_quadrature_rule, facet_quadrature_rule)
+struct CellGeometry
+    volume::Float64
+    face_areas::Vector{Float64}
+    centroid_coords::Vector{Float64}
+    unique_connections::Vector{Any}
+end
+
+function FVM_cell_geometries(grid, poly_interp, cell_quadrature_rule, facet_quadrature_rule)
     cell_qr = cell_quadrature_rule
     cell_values = CellValues(cell_qr, poly_interp)
     facet_qr = facet_quadrature_rule
     facet_values = FacetValues(facet_qr, poly_interp)
 
-    cell_dimensions = []
+    cell_geometries = Vector{CellGeometry}()
 
     for cell in CellIterator(grid)
         #Get cell volume
-        reinit!(cell_values, cell)
-        vol_i = 0.0 
+        Ferrite.reinit!(cell_values, cell)
+        vol = 0.0 
         for qp in 1:getnquadpoints(cell_values)
             d_vol = getdetJdV(cell_values, qp) 
-            vol_i += d_vol
+            vol += d_vol
         end
         
         #Get face areas
-        area = []
+        areas = []
         cell_coords = getcoordinates(cell)
         
         for facet_index in 1:nfacets(cell)
-            reinit!(facet_values, cell_coords, facet_index)
+            Ferrite.reinit!(facet_values, cell_coords, facet_index)
             
             area_i = 0.0
             for qp in 1:getnquadpoints(facet_values)
                 area_i += getdetJdV(facet_values, qp)
             end
             
-            push!(area, area_i)
+            push!(areas, area_i)
         end
 
         #Get centroid
-        x_avg = sum([c[1] for c in cell_coords]) / length(cell_coords)
-        y_avg = sum([c[2] for c in cell_coords]) / length(cell_coords)
-        z_avg = sum([c[3] for c in cell_coords]) / length(cell_coords)
+        centroid_vec = sum(cell_coords) / length(cell_coords)
 
-        centroid_coords = [x_avg, y_avg, z_avg]
+        centroid_coords = [centroid_vec[1], centroid_vec[2], centroid_vec[3]]
 
-        push!(cell_dimensions, [vol_i, [area], centroid_coords]) #formatting it this way because it's kindof 3D, 2D, 1D although a different format might be better
+        push!(cell_geometries, CellGeometry(vol, areas, centroid_coords, [])) #formatting it this way because it's kindof 3D, 2D, 1D although a different format might be better
     end
-    return cell_dimensions
+    
+    top = ExclusiveTopology(grid)
+    unique_connections = Set{Vector{Vector{Int}}}() #Set{Tuple{Tuple{Number, Number}, Tuple{Number, Number}}}() 
+
+    for cell in CellIterator(grid)
+        i = cellid(cell) 
+        for j in 1:nfacets(cell)
+            neighbors = top.face_face_neighbor[i, j]
+            if !isempty(neighbors)
+                push!(unique_connections, collect(minmax([i, j], collect(neighbors[1].idx))))
+            end
+        end
+    end
+
+    println("HI", unique_connections)
+
+    unique_connections = collect(unique_connections)
+
+    for i in 1:length(unique_connections) #should probably find a way to connect this with the loop above but I don't know how without a set to get rid of duplicates
+        cell_idx = unique_connections[i][1][1]
+        println(cell_idx)
+        push!(cell_geometries[cell_idx].unique_connections, unique_connections[i])
+    end
+    return cell_geometries
 end
 
 poly_interp = Lagrange{RefHexahedron, 1}() #1 = linear elements 2 = quadratic/curved edges
@@ -154,27 +186,8 @@ cell_qr = QuadratureRule{RefHexahedron}(2) #2 represents the number of integrati
 
 facet_qr = FacetQuadratureRule{RefHexahedron}(2) 
 
-cell_dimensions = generate_FVM_connectivity(grid, poly_interp, cell_qr, facet_qr)
-#END OF FERRITE SECION
+cell_geometries = FVM_cell_geometries(grid, poly_interp, cell_qr, facet_qr)
 
-n_cells = getncells(grid)
-
-cells = [HeatCapacitor(name=Symbol("node_", i), C=cell_dimensions[i][1]*1.7*1000000) for i in 1:n_cells] #get volume #using a test value of 1.7 J/(g*K) and 1000000g/m^3
-
-conds = [ThermalConductor(name=Symbol("h_cond_", i)) for facet_idx in 1:nfacets(getcells(grid)[i]) for cell_idx in 1:n_cells] # need a way to multiply U by the area between the cells
-#also, won't this triple up thermal conductors because each should really define 3 connections to other nodes
-#I guess we need a similar unique_connections to the FEM example
-
-#Add boundary condition components
-#thought this would be hard but getnodeset(grid, "some_boundary_condition_name") does exactly what I need
-n_left_bcs = length(collect(getcellset(grid, "left")))
-left_bcs = [SomeHeatSource(name=Symbol("heat_source_", i)) for i in 1:n_left_bcs] 
-n_right_bcs = length(collect(getcellset(grid, "right")))
-right_bcs = [SomeFixedTemperature(T_fixed=293.15, name=Symbol("fixed_temp_", i)) for i in 1:n_right_bcs]
-
-connections = Equation[]
-
-#START of small ferrite section
 function FVM_unique_connections(grid)
     top = ExclusiveTopology(grid)
     unique_connections = Set() #Set{Tuple{Tuple{Number, Number}, Tuple{Number, Number}}}() #don't know if not defning the set type decreases performance
@@ -191,12 +204,48 @@ function FVM_unique_connections(grid)
 end
 
 unique_connections = FVM_unique_connections(grid)
-#END of small ferrite section
-#unique_connections is formatted [[[cell_idx, ]...]
+#END OF FERRITE SECION
 
-for (i, ((cell_idx, facet_idx), (neighbor_idx, neighbor_facet_idx))) in enumerate(unique_connections)
-    push!(connections, connect(nodes[i_in].port, conds[i].port_a))
-    push!(connections, connect(conds[i].port_b, nodes[i_out].port))
+k_thermal = 200
+rho = 2700
+cp = 900
+vol_heat_cap = rho * cp
+
+n_cells = getncells(grid)
+
+cells = [HeatCapacitor(name=Symbol("node_", i), C=cell_geometries[i].volume*vol_heat_cap) for i in 1:n_cells] 
+
+conds = []
+
+for i in 1:length(cell_geometries)
+    for j in 1:length(cell_geometries[i].unique_connections)
+        face_area = cell_geometries[i].face_areas[cell_geometries[i].unique_connections[j][1][2]]
+        neighbor_cell_idx = cell_geometries[i].unique_connections[j][2][2]
+        x_dist = (cell_geometries[i].centroid_coords[1] + cell_geometries[neighbor_cell_idx].centroid_coords[1])
+        y_dist = (cell_geometries[i].centroid_coords[2] + cell_geometries[neighbor_cell_idx].centroid_coords[2])
+        z_dist = (cell_geometries[i].centroid_coords[3] + cell_geometries[neighbor_cell_idx].centroid_coords[3])
+        dist_between_cells = x_dist + y_dist + z_dist #not accurate but good enough for now
+        G_calc = k_thermal * face_area / dist_between_cells
+        push!(conds, ThermalConductor(name=Symbol("h_cond_", i, "_", j), G=cell_geometries[i].face_areas[j]), )
+    end
+end
+
+#conds = [ThermalConductor(name=Symbol("h_cond_")), G=cell.geometries[i] for cell_geom in cell_geometries for j in 1:length(cell_geom.unique_connections)] # need a way to multiply U by the area between the cells
+
+#Add boundary condition components
+#thought this would be hard but getnodeset(grid, "some_boundary_condition_name") does exactly what I need
+n_left_bcs = length(collect(getcellset(grid, "left")))
+left_bcs = [SomeHeatSource(name=Symbol("heat_source_", i), Q_flow_fixed=5000) for i in 1:n_left_bcs] 
+n_right_bcs = length(collect(getcellset(grid, "right")))
+right_bcs = [SomeFixedTemperature(name=Symbol("fixed_temp_", i), T_fixed=293.15) for i in 1:n_right_bcs]
+
+connections = Equation[]
+
+for i in 1:length(cell_geometries)
+    for j in 1:length(cell_geometries)
+        push!(connections, connect(cells[cell_idx].port, conds[facet_idx].port_a))
+        push!(connections, connect(conds[facet_idx].port_b, cells[neighbor_idx].port))
+    end
 end
 
 left_node_indicies = collect(getnodeset(grid, "left"))
